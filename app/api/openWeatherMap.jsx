dff3bb133297d3161b8d005ee5eda4a583a498ad
jamesycs <kaynejames@Kaynes-MacBook-Pro.local>
2017-07-03 18:59:52 +0800

    Added a comment
    make a constant variable:

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,5 +1,6 @@
 var axios = require('axios');
 
+//make a constant variable:
 const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c4e735ea8bd7e7b6dc8368c752517b2d&units=imperial';
 
 module.exports = {
